7a72ac6edb7567c02a9fb15fbe8577d64786f2f2
libing <libingdeepblue@163.com>
2019-03-12 13:06:40 +0800

    recursion e.g. fibonacci

--- basic/recursion.go
+++ basic/recursion.go
@@ -1,20 +1,34 @@
 package main
 
-import "fmt"
-
-func Factorial(n uint64) uint64 {
-	if (n > 0) {
-		result := n * Factorial(n-1)
-		return result
-	}
-	return 1
-}
+import (
+	"fmt"
+)
 
 func main() {
-	var i int = 15
-	fmt.Printf("%d 的阶乘是 %d\n", i, Factorial(uint64(i)))
+	fmt.Printf("%d is even: is %t\n", 16, even(16)) // 16 is even: is true
+	fmt.Printf("%d is odd: is %t\n", 17, odd(17))
+	// 17 is odd: is true
+	fmt.Printf("%d is odd: is %t\n", 18, odd(18))
+	// 18 is odd: is false
 }
 
-/*
-15 的阶乘是 1307674368000
-*/
+func even(nr int) bool {
+	if nr == 0 {
+		return true
+	}
+	return odd(RevSign(nr) - 1)
+}
+
+func odd(nr int) bool {
+	if nr == 0 {
+		return false
+	}
+	return even(RevSign(nr) - 1)
+}
+
+func RevSign(nr int) int {
+	if nr < 0 {
+		return -nr
+	}
+	return nr
+}
