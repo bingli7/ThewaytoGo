1fe815aa7789e2ca76da0b6be7acdd926053a4f7
libing <libingdeepblue@163.com>
2019-03-20 13:22:24 +0800

    more usage of struct

--- basic/struct_new.go
+++ basic/struct_new.go
@@ -25,4 +25,15 @@ func main() {
 	t3 := T{name: "vvv", age: 11}
 	fmt.Printf("%T\n", t3)
 	fmt.Printf("%s, %d\n", t3.name, t3.age)
+	// "." 在 Go 语言中叫 选择器（selector）。
+	// 无论变量是一个结构体类型还是一个结构体类型指针，都使用同样的 选择器符（selector-notation） 来引用结构体的字段
+	t4 := new(T)
+	t4.name = "haha"
+	// 以下两种方式等同
+	t4.age = 9999
+	(*t4).age = 7777
+	// 两种方式都可以访问struct的字段
+	fmt.Printf("%s, %s\n", t4.name, (*t4).name)
+	fmt.Printf("%d, %d\n", t4.age, (*t4).age)
+
 }
